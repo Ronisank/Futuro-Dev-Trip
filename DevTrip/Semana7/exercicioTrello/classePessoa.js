053bedfb6bc8b6f7712fdcce7f27d3b98ec7f186
Roni <ronisank21@hotmail.com>
2024-03-08 17:25:25 -0300

    fix exercises 7-8

--- DevTrip/Semana7/exercicioTrello/classePessoa.js
+++ DevTrip/Semana7/exercicioTrello/classePessoa.js
@@ -7,5 +7,21 @@ class Pessoa{
         this.nome = nome
         this.idade = idade
         this.profissao = profissao
-    }s
+    }
 }
+
+class Cliente extends Pessoa{
+    telefone;
+    email;
+    clienteDesde;
+
+    constructor(nome, idade, profissao, telefone, email, clienteDesde){
+        super(nome, idade, profissao)
+        this.telefone = telefone
+        this.email = email
+        this.clienteDesde = clienteDesde
+    }
+}
+
+let cli = new Cliente("João", 25, "Programador", "9999-9999","joao@joao", "2024-01-01")
+console.log(cli)
